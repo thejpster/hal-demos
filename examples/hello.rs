c1783e7740071d9f137097f3d5117b1494c5db39
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-01-28 21:46:11 +0000

    Get chip info.

--- examples/hello.rs
+++ examples/hello.rs
@@ -8,15 +8,25 @@
 extern crate cortex_m;
 extern crate cortex_m_rt;
 extern crate cortex_m_semihosting;
+extern crate tm4c123x_hal;
+extern crate tm4c123x;
 
 use core::fmt::Write;
 
 use cortex_m::asm;
 use cortex_m_semihosting::hio;
 
+use tm4c123x_hal::sysctl::SysctlExt;
+use tm4c123x_hal::sysctl::chip_id;
+
 fn main() {
+	let p = tm4c123x::Peripherals::take().unwrap();
+	let sysctl = SysctlExt::constrain(p.SYSCTL);
+	let clocks = sysctl.clock_setup.freeze();
     let mut stdout = hio::hstdout().unwrap();
     writeln!(stdout, "Hello, world!").unwrap();
+    writeln!(stdout, "Sysclk: {}", clocks.sysclk.0).unwrap();
+    writeln!(stdout, "Chip: {:?}", chip_id::get()).unwrap();
 }
 
 // As we are not using interrupts, we just register a dummy catch all handler
