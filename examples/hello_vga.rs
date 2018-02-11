e2ae0465a7031a48829f396aaee5b0e951de5560
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-02-11 14:35:20 +0000

    Timer0B triggers when line output needs to start.

--- examples/hello_vga.rs
+++ examples/hello_vga.rs
@@ -13,7 +13,6 @@ extern crate embedded_hal;
 
 use core::fmt::Write;
 use cortex_m::asm;
-use cortex_m_semihosting::hio;
 use embedded_hal::prelude::*;
 use tm4c123x_hal::gpio::GpioExt;
 use tm4c123x_hal::sysctl::{self, SysctlExt};
@@ -32,40 +31,43 @@ fn main() {
         &mut sc.power_control, sysctl::PeripheralPowerDomain::Timer0,
         sysctl::RunMode::Run, sysctl::PowerState::On);
     sysctl::reset(&mut sc.power_control, sysctl::PeripheralPowerDomain::Timer0);
-    let mut portb = p.GPIO_PORTB.split(&sc.power_control);
 
+    sysctl::control_power(
+        &mut sc.power_control, sysctl::PeripheralPowerDomain::Ssi2,
+        sysctl::RunMode::Run, sysctl::PowerState::On);
+    sysctl::reset(&mut sc.power_control, sysctl::PeripheralPowerDomain::Ssi2);
+
+    let mut portb = p.GPIO_PORTB.split(&sc.power_control);
     let mut h_sync = portb.pb6.into_af7(&mut portb.control);
+    // let mut h_timer = portb.pb7.into_af7(&mut portb.control);
     // let mut red_data = portb.pb2.into_push_pull_output();
     // let mut blue_data = portb.pb2.into_push_pull_output();
-    // let mut green_data = portb.pb2.into_push_pull_output();
+    let mut green_data = portb.pb7.info_af2(&mut portb.control); // Ssi2Tx
+
+    // Need to configure SSI2 at 20 MHz
+
+    // Need to configure MicroDMA to feed SSI2 with data, triggered on Timer0B interrupt
 
     let t0 = p.TIMER0;
-
-    // Configure T0A for h-sync
-    // Clear TnEN
+    // Configure T0A for h-sync and T0B for line trigger
     t0.ctl.modify(|_, w| w.taen().clear_bit().tben().clear_bit());
-    // CFG = 4 (16-bit mode)
     t0.cfg.modify(|_, w| w.cfg()._16_bit());
-    // TnMR.TnAMS = 1, TnCMR = 0, TnMR = 2 (Periodic, Edge Count, PWM)
     t0.tamr.modify(|_, w| w.taams().set_bit().tacmr().clear_bit().tamr().period());
-    // CTL.TnPWML = 0 (1 if it needs to be inverted)
+    t0.tbmr.modify(|_, w| w.tbams().set_bit().tbcmr().clear_bit().tbmr().period());
     t0.ctl.modify(|_, w| w.tapwml().clear_bit());
-    // TnPR = 0
+    t0.ctl.modify(|_, w| w.tbpwml().set_bit());
     t0.tapr.modify(|_, w| unsafe { w.bits(0) });
-    // TnILR = 528 (total horizontal clocks)
+    t0.tbpr.modify(|_, w| unsafe { w.bits(0) });
+    // TnILR = 528 (total horizontal pixels at 20 MHz)
     t0.tailr.modify(|_, w| unsafe { w.bits(528 * 4) });
-    // TAMATCHR = 528 - 64 = 464 (sync end)
+    t0.tbilr.modify(|_, w| unsafe { w.bits(528 * 4) });
+    // TAMATCHR = 528 - 64 = 464 (sync end) at 20 MHz
     t0.tamatchr.modify(|_, w| unsafe { w.bits(464 * 4) });
-    // TBMATCHR = 528 - 108 = 420 (line start)
-    // IMR.TBMIM = 1 (for line start)
-    // IMR.TBTOIM = 1 (for row count)
-    t0.ctl.modify(|_, w| w.taen().set_bit());
-    // CTL.TAEN = 1
-    // CTL.TBEN = 1
-    // Need a static atomic for line count
-
-    let mut itm = hio::hstdout().unwrap();
-    writeln!(itm, "Running at {} Hz", clocks.sysclk.0).unwrap();
+    // TBMATCHR = 528 - 108 = 420 (line start) at 20 MHz
+    t0.tbmatchr.modify(|_, w| unsafe { w.bits(420 * 4) });
+    // TODO IMR.TBMIM = 1 (for line start)
+    // TODO IMR.TBTOIM = 1 (for row count)
+    t0.ctl.modify(|_, w| w.taen().set_bit().tben().set_bit());
 }
 
 // As we are not using interrupts, we just register a dummy catch all handler
